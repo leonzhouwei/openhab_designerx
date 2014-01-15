0258500b7ee753d96008a89fda019fdee007b2a7
zhouwei <leonzhouwei@gmail.com>
2014-01-15 22:15:39 +0800

    ItemXtdex has passed its unit test

---
+++ bundles/designerx/model/org.openhab.designerx.model/src/main/java/org/openhab/designerx/model/ModelConstants.java
@@ -0,0 +1,34 @@
+package org.openhab.designerx.model;
+
+public class ModelConstants {
+	
+	// System
+	public static final String CURRENT_WORKING_DIR = System.getProperty("user.dir");
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	public static final String USER_HOME_DIR = System.getProperty("user.home");
+
+	// charset
+	public static final String CHARSET_NAME_UTF_8 = "utf-8";
+	
+	// marks
+	public static final String COMMA_MARK = ",";
+	public static final String SPACE_MARK = " ";
+	
+	// openHAB
+	public static final String ICON = "icon";
+	public static final String ITEMS_FILE_EXTENSION = ".items";
+	public static final String LABEL = "label";
+	public static final String OPENHAB_CONFIG_DIR_BASE_NAME = "configurations";
+	public static final String OPENHAB_ITEMS_DIR_BASE_NAME = "items";
+	public static final String OPENHAB_RULES_DIR_BASE_NAME = "rules";
+	public static final String OPENHAB_SCRIPTS_DIR_BASE_NAME = "scripts";
+	public static final String OPENHAB_SITEMAPS_DIR_BASE_NAME = "sitemaps";
+	public static final String OPENHAB_SITEMAPS_DIR_RELATIVE_PATH = ModelConstants.FILE_SEPARATOR + OPENHAB_CONFIG_DIR_BASE_NAME + FILE_SEPARATOR + OPENHAB_SITEMAPS_DIR_BASE_NAME;
+	public static final String OPENHAB_TRANSFORM_DIR_BASE_NAME = "transform";
+	public static final String OPENHAB_ITEMS_DIR_RELATIVE_PATH = ModelConstants.FILE_SEPARATOR + OPENHAB_CONFIG_DIR_BASE_NAME + FILE_SEPARATOR + OPENHAB_ITEMS_DIR_BASE_NAME;
+	public static final String SITEMAP = "sitemap";
+	public static final String SITEMAP_FILE_EXTENSION = ".sitemap";
+	
+	// DesignerFX
+}
