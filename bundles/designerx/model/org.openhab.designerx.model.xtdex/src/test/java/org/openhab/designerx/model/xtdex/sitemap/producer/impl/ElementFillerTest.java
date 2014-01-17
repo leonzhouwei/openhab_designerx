29458f8a4fe1a336b109aa7bc93282f7fcbb6293
zhouwei <leonzhouwei@gmail.com>
2014-01-17 14:19:12 +0800

    refine/add unit tests

--- bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ElementFillerTest.java
+++ bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ElementFillerTest.java
@@ -7,16 +7,18 @@ import org.junit.Test;
 import org.openhab.designerx.model.sitemap.Chart;
 import org.openhab.designerx.model.sitemap.producer.ElementFactory;
 import org.openhab.designerx.model.sitemap.producer.impl.ElementFactoryImpl;
+import org.openhab.designerx.model.xtdex.ModelXtdexException;
 
-public class ElementXtextualizerTest {
+public class ElementFillerTest {
 	private static ElementFactory factory = new ElementFactoryImpl();
 
 	@Test
-	public void testToXtextIgnoringChildren() {
+	public void testFillWithoutChildren() throws ModelXtdexException {
 		final String item = "lalala";
 		final String expected = "Chart item=" + item;
+		NonNestableElementXtextKeeper keeper = new NonNestableElementXtextKeeper(expected);
 		Chart e = factory.createChart();
-		e.setItem(item);
+		ElementFiller.fillWithoutChildren(e, keeper);
 		final String actual = ElementXtextualizer.toXtextIgnoringChildren(e);
 		assertThat(actual, Matchers.equalTo(expected));
 	}
