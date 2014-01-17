a5e5fd43dc3e1e4524aa0f5f191a4124cfbfafad
zhouwei <leonzhouwei@gmail.com>
2014-01-17 15:04:21 +0800

    add unit tests

--- bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ElementFillerTest.java
+++ bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ElementFillerTest.java
@@ -12,9 +12,9 @@ import org.openhab.designerx.model.xtdex.ModelXtdexException;
 public class ElementFillerTest {
 	private static ElementFactory factory = new ElementFactoryImpl();
 	
-//	@Test
+	@Test
 	public void testFillWithoutChildren_1() throws ModelXtdexException {
-		final String expected = "Chart item=Weather_Chart";
+		final String expected = "Chart";
 		NonNestableElementXtextKeeper keeper = new NonNestableElementXtextKeeper(expected);
 		Chart e = factory.createChart();
 		ElementFiller.fillWithoutChildren(e, keeper);
@@ -22,9 +22,9 @@ public class ElementFillerTest {
 		assertThat(actual, Matchers.equalTo(expected));
 	}
 
-//	@Test
+	@Test
 	public void testFillWithoutChildren_2() throws ModelXtdexException {
-		final String expected = "Chart item=Weather_Chart valuecolor=[Weather_LastUpdate==\"Uninitialized\"=\"lightgray\",Weather_LastUpdate>90=\"lightgray\",>25=\"orange\",>15=\"green\",>5=\"orange\",<=5=\"blue\"]";
+		final String expected = "Chart item=Weather_Chart";
 		NonNestableElementXtextKeeper keeper = new NonNestableElementXtextKeeper(expected);
 		Chart e = factory.createChart();
 		ElementFiller.fillWithoutChildren(e, keeper);
@@ -34,7 +34,17 @@ public class ElementFillerTest {
 	
 	@Test
 	public void testFillWithoutChildren_3() throws ModelXtdexException {
-		final String expected = "Chart item=Weather_Chart visibility=[Weather_Chart_Period==0,Weather_Chart_Period==\"Uninitialized\"]";
+		final String expected = "Chart item=Weather_Chart valuecolor=[Weather_LastUpdate==\"Uninitialized\"=\"lightgray\",Weather_LastUpdate>90=\"lightgray\",>25=\"orange\",>15=\"green\",>5=\"orange\",<=5=\"blue\"]";
+		NonNestableElementXtextKeeper keeper = new NonNestableElementXtextKeeper(expected);
+		Chart e = factory.createChart();
+		ElementFiller.fillWithoutChildren(e, keeper);
+		final String actual = ElementXtextualizer.toXtextIgnoringChildren(e);
+		assertThat(actual, Matchers.equalTo(expected));
+	}
+	
+	@Test
+	public void testFillWithoutChildren_4() throws ModelXtdexException {
+		final String expected = "Chart item=Weather_Chart valuecolor=[Weather_LastUpdate==\"Uninitialized\"=\"lightgray\",Weather_LastUpdate>90=\"lightgray\",>25=\"orange\",>15=\"green\",>5=\"orange\",<=5=\"blue\"] visibility=[Weather_Chart_Period==0,Weather_Chart_Period==\"Uninitialized\"]";
 		NonNestableElementXtextKeeper keeper = new NonNestableElementXtextKeeper(expected);
 		Chart e = factory.createChart();
 		ElementFiller.fillWithoutChildren(e, keeper);
