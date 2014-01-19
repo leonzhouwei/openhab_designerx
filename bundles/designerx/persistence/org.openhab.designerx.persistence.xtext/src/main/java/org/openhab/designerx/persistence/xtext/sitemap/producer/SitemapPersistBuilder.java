a4886b5ca54f398fc8766cd23871ad5c413e741e
zhouwei <leonzhouwei@gmail.com>
2014-01-19 15:05:15 +0800

    updated

--- bundles/designerx/persistence/org.openhab.designerx.persistence.xtext/src/main/java/org/openhab/designerx/persistence/xtext/sitemap/producer/SitemapPersistBuilder.java
+++ bundles/designerx/persistence/org.openhab.designerx.persistence.xtext/src/main/java/org/openhab/designerx/persistence/xtext/sitemap/producer/SitemapPersistBuilder.java
@@ -1,9 +1,15 @@
 package org.openhab.designerx.persistence.xtext.sitemap.producer;
 
+import org.openhab.designerx.config.ConfigException;
 import org.openhab.designerx.persistence.xtext.sitemap.SitemapPersist;
 
 public interface SitemapPersistBuilder {
 	
-	public SitemapPersist build(String name);
+	/**
+	 * 
+	 * @param name  e.g. 'demo' for $OPENHAB_HOME/configurations/sitemaps/demo.sitemap
+	 * @return
+	 */
+	public SitemapPersist build(String name) throws ConfigException;
 
 }
--- bundles/designerx/persistence/org.openhab.designerx.persistence.xtext/src/main/java/org/openhab/designerx/persistence/xtext/sitemap/producer/impl/SitemapPersistBuilderImpl.java
+++ bundles/designerx/persistence/org.openhab.designerx.persistence.xtext/src/main/java/org/openhab/designerx/persistence/xtext/sitemap/producer/impl/SitemapPersistBuilderImpl.java
@@ -1,14 +1,40 @@
 package org.openhab.designerx.persistence.xtext.sitemap.producer.impl;
 
+import org.openhab.designerx.config.Config;
+import org.openhab.designerx.config.ConfigBuilder;
+import org.openhab.designerx.config.ConfigException;
+import org.openhab.designerx.model.sitemap.Sitemap;
 import org.openhab.designerx.persistence.xtext.sitemap.SitemapPersist;
 import org.openhab.designerx.persistence.xtext.sitemap.producer.SitemapPersistBuilder;
 
 public final class SitemapPersistBuilderImpl implements SitemapPersistBuilder {
 
 	@Override
-	public SitemapPersist build(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public SitemapPersist build(String name) throws ConfigException {
+		return new SitemapPersistImpl(name);
+	}
+	
+	private class SitemapPersistImpl implements SitemapPersist {
+		private Config config;
+		private String name;
+		
+		public SitemapPersistImpl(String name) throws ConfigException {
+			config = ConfigBuilder.build();
+			this.name = name;
+		}
+
+		@Override
+		public Sitemap get() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void save(Sitemap sitemap) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 }
