955c9f10502a8920c2263e2b38b7ab49435b912c
zhouwei <leonzhouwei@gmail.com>
2014-01-20 11:24:53 +0800

    refine the unit tests

---
+++ bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/items/producer/impl/ItemXtdexImplTest.java
@@ -0,0 +1,96 @@
+package org.openhab.designerx.model.xtdex.items.producer.impl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.openhab.designerx.model.ModelException;
+import org.openhab.designerx.model.items.Item;
+import org.openhab.designerx.model.xtdex.items.ItemXtdex;
+import org.openhab.designerx.model.xtdex.items.producer.ItemXtdexBuilder;
+
+public class ItemXtdexImplTest {
+	private static final ItemXtdexBuilder builder = new ItemXtdexBuilderImpl();
+	private static final ItemXtdex xtdex = builder.build();
+
+	@Test
+	public void testParseColorItem() throws ModelException {
+		final String xtext = "Color RGBLight \"RGB Light\" <slider>";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseContactItem() throws ModelException {
+		final String xtext = "Contact Window_GF_Frontdoor \"Frontdoor [MAP(en.map):%s]\" (GF_Corridor,Windows)";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseDateTimeItem() throws ModelException {
+		final String xtext = "DateTime Date \"Date [%1$tA, %1$td.%1$tm.%1$tY]\" <calendar> {ntp=\"Europe/Berlin:de_DE\"}";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseDimmerItem() throws ModelException {
+		final String xtext = "Dimmer DimmedLight \"Dimmer [%d %%]\" <slider>";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseGroupItem_1() throws ModelException {
+		final String xtext = "Group GF_Living \"Living Room\" <video> (gGF)";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParseGroupItem_2() throws ModelException {
+		final String xtext = "Group:Switch:OR(ON, OFF) Heating \"No. of Active Heatings [(%d)]\" <heating> (All)";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseNumberItem() throws ModelException {
+		final String xtext = "Number Temperature_GF_Corridor \"Temperature [%.1f °C]\" <temperature> (Temperature,GF_Corridor)";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseRollershutterItem() throws ModelException {
+		final String xtext = "Rollershutter DemoShutter \"Roller Shutter\"";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseStringItem() throws ModelException {
+		final String xtext = "String UnknownDevices \"Unknown Devices in Range: [%s]\" {bluetooth=\"?\"}";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+	@Test
+	public void testParseSwitchItem() throws ModelException {
+		final String xtext = "Switch Light_GF_Corridor_Ceiling \"Ceiling\" (GF_Corridor,Lights)";
+		Item item = xtdex.parseItem(xtext);
+		final String actual = xtdex.toXtext(item);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+
+}
--- bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ChildlessElementXtdexTest.java
+++ bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ChildlessElementXtdexTest.java
@@ -1,16 +1,39 @@
 package org.openhab.designerx.model.xtdex.sitemap.producer.impl;
 
-import org.openhab.designerx.model.sitemap.Video;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.openhab.designerx.model.sitemap.Element;
 import org.openhab.designerx.model.xtdex.ModelXtdexException;
 
 public class ChildlessElementXtdexTest {
+	
+	@Test
+	public void testParse_1() throws ModelXtdexException {
+		final String xtext = "Chart item=Weather_Chart visibility=[Weather_Chart_Period==0,Weather_Chart_Period==\"Uninitialized\"] period=h refresh=600";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_2() throws ModelXtdexException {
+		final String xtext = "Colorpicker item=RGBLight icon=\"slider\" sendFrequency=0";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
 
-//	@Test
-	public void testParse() throws ModelXtdexException {
+	@Test
+	public void testParse_12() throws ModelXtdexException {
 		final String xtext = "Video url=\"http://demo.openhab.org/Hue.m4v\"";
 		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
-		Video video = (Video) ChildlessElementXtdex.parse(keeper);
-		System.out.println(VideoXtdex.toXtextIgnoringChildren(video));
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
 	}
 
 }
