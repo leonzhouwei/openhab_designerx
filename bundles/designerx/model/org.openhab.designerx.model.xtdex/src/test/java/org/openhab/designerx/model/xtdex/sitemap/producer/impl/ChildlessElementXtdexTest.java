e856cde0050aaca4356db5d23858f926c28715bd
zhouwei <leonzhouwei@gmail.com>
2014-01-20 11:30:21 +0800

    sitemap childless elements unit test done

--- bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ChildlessElementXtdexTest.java
+++ bundles/designerx/model/org.openhab.designerx.model.xtdex/src/test/java/org/openhab/designerx/model/xtdex/sitemap/producer/impl/ChildlessElementXtdexTest.java
@@ -26,10 +26,55 @@ public class ChildlessElementXtdexTest {
 		final String actual = ChildlessElementXtdex.toXtext(e);
 		assertThat(actual, Matchers.equalTo(xtext));
 	}
-
+	
 	@Test
-	public void testParse_12() throws ModelXtdexException {
+	public void testParse_3() throws ModelXtdexException {
+		final String xtext = "List item=aList";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_4() throws ModelXtdexException {
+		final String xtext = "Selection item=Radio_Station mappings=[0=off,1=HR3,2=SWR3,3=FFH]";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_5() throws ModelXtdexException {
+		final String xtext = "Setpoint item=Temperature_Setpoint minValue=16 maxValue=28 step=0.5";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_6() throws ModelXtdexException {
+		final String xtext = "Switch item=Scene_General label=\"Scene\" mappings=[1=TV,2=Dinner,3=Reading]";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_7() throws ModelXtdexException {
 		final String xtext = "Video url=\"http://demo.openhab.org/Hue.m4v\"";
+		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
+		Element e = ChildlessElementXtdex.parse(keeper);
+		final String actual = ChildlessElementXtdex.toXtext(e);
+		assertThat(actual, Matchers.equalTo(xtext));
+	}
+	
+	@Test
+	public void testParse_8() throws ModelXtdexException {
+		final String xtext = "Webview url=\"http://heise-online.mobi/\" height=8";
 		ChildlessElementXtextKeeper keeper = new ChildlessElementXtextKeeper(xtext);
 		Element e = ChildlessElementXtdex.parse(keeper);
 		final String actual = ChildlessElementXtdex.toXtext(e);
