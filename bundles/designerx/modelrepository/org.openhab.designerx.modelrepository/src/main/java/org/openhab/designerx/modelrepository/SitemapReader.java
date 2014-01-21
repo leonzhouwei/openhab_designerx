7ea4b7577dd22eb7b6d2a142491cadcf9265aebf
zhouwei <leonzhouwei@gmail.com>
2014-01-21 21:06:55 +0800

    delete the modelrepository module

--- bundles/designerx/modelrepository/org.openhab.designerx.modelrepository/src/main/java/org/openhab/designerx/modelrepository/SitemapReader.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.designerx.modelrepository;
-
-import org.openhab.designerx.model.sitemap.Sitemap;
-
-public interface SitemapReader {
-	
-	public Sitemap read();
-
-}
--- bundles/designerx/modelrepository/org.openhab.designerx.modelrepository/src/main/java/org/openhab/designerx/modelrepository/SitemapWriter.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.designerx.modelrepository;
-
-import org.openhab.designerx.model.sitemap.Sitemap;
-
-public interface SitemapWriter {
-
-	public void write(Sitemap sitemap);
-	
-}
