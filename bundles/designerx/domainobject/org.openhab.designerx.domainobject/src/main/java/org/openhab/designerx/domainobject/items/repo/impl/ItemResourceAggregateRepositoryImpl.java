29e75ec1933b286b7bed37333e24380a62945475
zhouwei <leonzhouwei@gmail.com>
2014-01-22 15:10:13 +0800

    updated

--- bundles/designerx/domainobject/org.openhab.designerx.domainobject/src/main/java/org/openhab/designerx/domainobject/items/repo/impl/ItemResourceAggregateRepositoryImpl.java
+++ bundles/designerx/domainobject/org.openhab.designerx.domainobject/src/main/java/org/openhab/designerx/domainobject/items/repo/impl/ItemResourceAggregateRepositoryImpl.java
@@ -3,20 +3,20 @@ package org.openhab.designerx.domainobject.items.repo.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.openhab.designerx.domainobject.DomainObjectException;
 import org.openhab.designerx.domainobject.items.ItemResourceAggregate;
 import org.openhab.designerx.domainobject.items.repo.ItemResourceAggregateRepository;
-import org.openhab.designerx.persistence.xtext.items.XtextItemResource;
 import org.openhab.designerx.persistence.xtext.items.repo.XtextItemResourceRepository;
 import org.openhab.designerx.persistence.xtext.items.repo.impl.XtextItemResourceRepositoryFactory;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 final class ItemResourceAggregateRepositoryImpl implements ItemResourceAggregateRepository {
 	private XtextItemResourceRepository xtextRepo = XtextItemResourceRepositoryFactory.create();
-	
+
 	private Map<String, ItemResourceAggregate> map = Maps.newHashMap();
-	private static final ItemResourceAggregateRepository instance = new ItemResourceAggregateRepositoryImpl();	
+	private static long nextId = 1; 
+	private static final ItemResourceAggregateRepositoryImpl instance = new ItemResourceAggregateRepositoryImpl();	
 	
 	@Override
 	public ItemResourceAggregate getByName(String name) {
@@ -24,10 +24,24 @@ final class ItemResourceAggregateRepositoryImpl implements ItemResourceAggregate
 	}
 	
 	private ItemResourceAggregateRepositoryImpl() {
-		List<String> names = Lists.newArrayList();
+		List<String> names = xtextRepo.listNames();
 		for (String name : names) {
-			XtextItemResource xir = xtextRepo.find(name);
+			ItemResourceAggregate ira = new ItemResourceAggregateImpl(id, name);
+			map.put(name, ira);
 		}
+	}
+	
+	static ItemResourceAggregateRepositoryImpl getInstance() {
+		return instance; 
+	}
+	
+	synchronized long generateId() {
+		long ret = nextId;
+		nextId += 1;
+		if (nextId <= 0) {
+			throw new DomainObjectException("no available ");
+		}
+		return ret;
 	}
 	
 }
