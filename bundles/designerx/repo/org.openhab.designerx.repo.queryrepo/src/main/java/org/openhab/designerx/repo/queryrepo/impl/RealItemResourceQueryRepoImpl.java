8c19fdad42ad746561751d4b5529702e3eeced26
zhouwei <leonzhouwei@gmail.com>
2014-01-29 19:22:50 +0800

    [debug] fill the cache in the constructor method

--- bundles/designerx/repo/org.openhab.designerx.repo.queryrepo/src/main/java/org/openhab/designerx/repo/queryrepo/impl/RealItemResourceQueryRepoImpl.java
+++ bundles/designerx/repo/org.openhab.designerx.repo.queryrepo/src/main/java/org/openhab/designerx/repo/queryrepo/impl/RealItemResourceQueryRepoImpl.java
@@ -4,23 +4,20 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.openhab.designerx.model.ModelException;
 import org.openhab.designerx.model.items.ItemResource;
-import org.openhab.designerx.model.xtdex.ModelXtdexException;
 import org.openhab.designerx.model.xtdex.items.ItemResourceXtdex;
 import org.openhab.designerx.model.xtdex.items.impl.ItemResourceXtdexImpl;
 import org.openhab.designerx.persistence.xtext.items.ItemResourceQuery;
-import org.openhab.designerx.persistence.xtext.items.impl.ItemResourceQueryImpl;
+import org.openhab.designerx.persistence.xtext.items.repo.impl.ItemResourceQueryImpl;
 import org.openhab.designerx.repo.queryrepo.ItemResourceQueryRepo;
-import org.openhab.designerx.repo.queryrepo.QueryRepoException;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-final class RealItemResourceQueryRepoImpl implements
-		ItemResourceQueryRepo {
+final class RealItemResourceQueryRepoImpl implements ItemResourceQueryRepo {
 	private ItemResourceXtdex xtdex = new ItemResourceXtdexImpl();
 	private static Map<String, ItemResource> map = Maps.newHashMap();
+	
 	private static final RealItemResourceQueryRepoImpl instance = new RealItemResourceQueryRepoImpl();
 
 	public static RealItemResourceQueryRepoImpl getInstance() {
@@ -37,7 +34,7 @@ final class RealItemResourceQueryRepoImpl implements
 				list.add(replica);
 			}
 		} catch (Exception e) {
-			throw new QueryRepoException(e);
+			throw new RuntimeException(e);
 		}
 		return list;
 	}
@@ -50,29 +47,31 @@ final class RealItemResourceQueryRepoImpl implements
 				replica = createReplica(replica);
 			}
 		} catch (Exception e) {
-			throw new QueryRepoException(e);
+			throw new RuntimeException(e);
 		}
 		return replica;
 	}
 
 	private RealItemResourceQueryRepoImpl() {
-		ItemResourceQuery query = new ItemResourceQueryImpl();
 		try {
-			List<String> names = query.nameList();
-			for (String name : names) {
-				ItemResource value;
-				value = query.byName(name);
-				map.put(name, value);
+			ItemResourceQuery query = new ItemResourceQueryImpl();
+			List<ItemResource> list = query.replicas();
+			for (ItemResource e : list) {
+				map.put(e.name(), e);
 			}
 		} catch (Exception e) {
-			throw new QueryRepoException(e);
+			throw new RuntimeException(e);
 		}
 	}
 
-	private ItemResource createReplica(ItemResource source)
-			throws ModelXtdexException, ModelException {
-		String xtext = xtdex.toXtext(source);
-		ItemResource replica = xtdex.fromXtext(xtext);
+	private ItemResource createReplica(ItemResource source) {
+		ItemResource replica = null;
+		try {
+			String xtext = xtdex.toXtext(source);
+			replica = xtdex.fromXtext(source.name(), xtext);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 		return replica;
 	}
 
